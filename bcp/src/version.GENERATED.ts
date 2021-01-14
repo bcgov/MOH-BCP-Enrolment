bf63694166409c1f73f50ca07a973818218b6a1b
Harry Maynard <harry.maynard@maximusbc.ca>
2021-01-14 17:45:46 -0500

    Added version.GENERATED.ts file.

--- bcp/src/version.GENERATED.ts
+++ bcp/src/version.GENERATED.ts
@@ -1,9 +1,9 @@
 // DO NOT DELETE OR APP WILL FAIL TO COMPILE! Generated from version.js
-export const gitCommit = '9628f88';
-export const buildTime = '2/10/2020 at 9:35:35 AM';
+export const gitCommit = '47678c2';
+export const buildTime = '1/14/2021 at 2:43:04 PM';
 /** App version retrieved from package.json. */
-export const projectVersion = '1.0.0';
+export const projectVersion = '2.3.0';
 /** Human readable message  */
-export const message = 'Canonical Version: 9628f88 - 2/10/2020 at 9:35:35 AM (v1.0.0) \nhttps://github.com/bcgov/moh-bcp/commits/9628f88';
+export const message = 'Canonical Version: 47678c2 - 1/14/2021 at 2:43:04 PM (v2.3.0) \nhttps://github.com/bcgov/moh-bcp/commits/47678c2';
 /** If true, other values should be present. */
 export const success = true;
