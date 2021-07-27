4b83a9843a0e222f4b1a885c38376576cd5b78a1
Ian-c-cameron <ian.c.cam@gmail.com>
2021-07-27 13:29:30 -0700

    fix: properly add recaptcha-service

---
+++ recaptcha-service/src/loggerSetup.ts
@@ -0,0 +1,24 @@
+const winston = require('winston');
+const { HOSTNAME, LOG_LEVEL, WINSTON_HOST, WINSTON_PORT, } = require('./envConfig');
+////////////////////////////////////////////////////////
+/*
+ * Logger init
+ */
+////////////////////////////////////////////////////////
+winston.level = LOG_LEVEL
+winston.remove(winston.transports.Console)
+winston.add(winston.transports.Console, {
+  'timestamp': true
+})
+if (WINSTON_PORT) {
+  winston.add(winston.transports.Syslog, {
+    host: WINSTON_HOST,
+    port: WINSTON_PORT,
+    protocol: 'udp4',
+    localhost: HOSTNAME
+  })
+}
+
+module.exports = () => {
+  return winston;
+}
